c07d3ecb18f8a0ec1febf6c6124018321ff47de4
jsj3282 <jeongseonju15@gmail.com>
2025-08-14 18:48:03 +0900

    feat : PDF 다운로드 기능 버그 수정

---
+++ src/web/utils/fonts/NotoSansKR-Regular-normal.js
@@ -0,0 +1,11 @@
+// This is a placeholder for the font file
+// For proper Korean support, you need to:
+// 1. Download NotoSansKR font from Google Fonts
+// 2. Convert it to base64 using a tool like https://www.giftofspeed.com/base64-encoder/
+// 3. Replace the fontData below with the actual base64 string
+
+export const NotoSansKRRegular = {
+  fontName: 'NotoSansKR-Regular',
+  fontStyle: 'normal',
+  fontData: '' // Add base64 encoded font data here
+};
